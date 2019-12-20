8dcd8879a81021e73cb5f85fe0c080dd44e3d0c2
fdango <ccguangyao@gmail.com>
2019-12-20 14:46:50 +0800

    Add smart contract address list (keccak256, commit)

---
+++ bin/list.js
@@ -0,0 +1,6 @@
+var soljsonSources = [
+    "soljson_v0.4.24.js+commit.6ae29c7",
+    "soljson_v0.4.25.js+commit.69a1e72",
+    "soljson_v0.5.4.js+commit.1d7181d",
+    "soljson_v0.5.8.js+commit.1f148fe"
+]
